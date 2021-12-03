4671189841ffaef6934311c4309fdd002631d06a
MaxF8 <64173856+MaxF8@users.noreply.github.com>
2021-12-03 12:53:58 -0500

    Update SmartCityApplication.java

--- Smart-City/SpringBoot/smartcity/src/main/java/com/smartcity/SmartCityApplication.java
+++ Smart-City/SpringBoot/smartcity/src/main/java/com/smartcity/SmartCityApplication.java
@@ -9,10 +9,6 @@ import java.util.Map;
 public class SmartCityApplication {
 
 	public static void main(String[] args) {
-		/*Map<String, String> map = System.getenv();
-		for (Map.Entry <String, String> entry: map.entrySet()) {
-			System.out.println("Variable Name:- " + entry.getKey() + " Value:- " + entry.getValue());
-		}*/
 		SpringApplication.run(SmartCityApplication.class, args);
 	}
 
